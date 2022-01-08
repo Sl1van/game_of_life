36f2858a6066cedde9e6b8b3873c26ed9a453dc3
Sl1van <58076574+Sl1van@users.noreply.github.com>
2022-01-09 00:28:00 +0100

    added swt dependency and created basic swt window

--- src/main/java/ch/bbw/game_of_life/Main.java
+++ src/main/java/ch/bbw/game_of_life/Main.java
@@ -1,5 +1,18 @@
 package ch.bbw.game_of_life;
 
-public class Main {
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 
+public class Main {
+    public static void main(String[] args) {
+        Display display = new Display();
+        Shell shell = new Shell(display);
+        shell.setText("Game of life");
+        shell.open();
+        while (!shell.isDisposed()) {
+            if (!display.readAndDispatch())
+            { display.sleep();}
+        }
+        display.dispose();
+    }
 }
